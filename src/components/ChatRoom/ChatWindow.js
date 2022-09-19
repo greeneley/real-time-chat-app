671d744d8a801c11dcd1d9295a818cb5934c4f02
thanh hai <hai.thanh.dinh@mgm-tp.com>
2022-09-19 22:47:11 +0700

    38:28

--- src/components/ChatRoom/ChatWindow.js
+++ src/components/ChatRoom/ChatWindow.js
@@ -1,5 +1,54 @@
+import { UserAddOutlined } from "@ant-design/icons";
+import { Avatar, Button, Tooltip } from "antd";
 import React from "react";
+import styled from "styled-components";
 
+const HeaderStyled = styled.div`
+  display: flex;
+  justify-content: space-between;
+  height: 56px;
+  padding: 0 16px;
+  align-items: center;
+  border-bottom: 1px solid rgb(230, 230, 230);
+
+  .header {
+    &__info {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;f
+    }
+  }
+`;
+
+const ContentStyled = styled.div``;
 export default function ChatWindow() {
-  return <div>ChatWindow</div>;
+  return (
+    <div>
+      <HeaderStyled>
+        <div className="header__info">
+          <p className="header__title">Room 1</p>
+          <span className="header__description">Day la room 1</span>
+        </div>
+        <div>
+          <Button icon={<UserAddOutlined />} />
+          <Avatar.Group
+            size="small"
+            maxCount={2}
+            maxStyle={{ color: "#f56a00", backgroundColor: "#fde3cf" }}
+          >
+            <Tooltip title="A">
+              <Avatar>A</Avatar>
+            </Tooltip>
+            <Tooltip title="B">
+              <Avatar>B</Avatar>
+            </Tooltip>
+            <Tooltip title="C">
+              <Avatar>C</Avatar>
+            </Tooltip>
+          </Avatar.Group>
+        </div>
+      </HeaderStyled>
+      <ContentStyled></ContentStyled>
+    </div>
+  );
 }
